7f40b374b6ab1fc6509bed5c455e5c5e69d4c9c3
jerry-tab <117555919+jerry-tab@users.noreply.github.com>
2023-10-03 07:30:31 +0530

    Add files via upload

---
+++ alpha.java
@@ -0,0 +1,21 @@
+public class alpha
+{
+    public static void main(String args[])
+    {
+        int num1=65;
+        for(int i=0;i<26;i++){
+            System.out.print((char)(num1+i));
+        }
+        System.out.println();
+        int num2=97;
+        for(int i=0;i<26;i++){
+            System.out.print((char)(num2+i));
+        }
+        System.out.println();
+        char char1='A';
+        System.out.println((int)char1);
+
+
+
+    }
+}

c0469082b43d8ede35daa212e82dbdc7057c3735
Tejashwini A B <117555919+Tejashwini-AB@users.noreply.github.com>
2023-11-29 21:20:46 +0530

    Update T2.java

--- NumToFiginKan/T2.java
+++ NumToFiginKan/T2.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class T2 {
 	public static void main(String[] args) throws FileNotFoundException {
-		System.setProperty("file.encoding", "UTF-8");
 		Scanner scanner = new Scanner(new File("knum.txt"));
 		while (scanner.hasNextLine()) {
 		    String line = scanner.nextLine();

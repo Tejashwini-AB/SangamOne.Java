b22734adfeb7dedefcf283fd14a75172bb670cbf
Tejashwini A B <117555919+Tejashwini-AB@users.noreply.github.com>
2024-06-04 07:22:34 +0530

    Add files via upload

--- perandcom/Perm1.java
+++ perandcom/Perm1.java
@@ -30,11 +30,72 @@ public class Perm1 {
 		for(int i=0;i<6;i++) {
 			list4.add(word1+comb4.get(i));
 		}	
-		System.out.println(list4);
+		word1=c2;
+		word2=c1+c3+c4;
+		comb4=perm3(word2);
+		for(int i=0;i<6;i++) {
+			list4.add(word1+comb4.get(i));
+		}
+		word1=c3;
+		word2=c1+c2+c4;
+		comb4=perm3(word2);
+		for(int i=0;i<6;i++) {
+			list4.add(word1+comb4.get(i));
+		}
+		word1=c4;
+		word2=c1+c2+c3;
+		comb4=perm3(word2);
+		for(int i=0;i<6;i++) {
+			list4.add(word1+comb4.get(i));
+		}
 		return list4;	
 	}
+	public static ArrayList perm5(String s5) {
+		ArrayList<String> comb5=new ArrayList<String>();
+		ArrayList<String> list5=new ArrayList<String>();
+		String c1=s5.substring(0,1);
+		String c2=s5.substring(1,2);
+		String c3=s5.substring(2,3);
+		String c4=s5.substring(3,4);
+		String c5=s5.substring(4,5);
+		
+		String word1=c1;
+		String word2=c2+c3+c4+c5;
+		comb5=perm4(word2);
+		for(int i=0;i<24;i++) {
+			list5.add(word1+comb5.get(i));
+		}
+		word1=c2;
+		word2=c1+c3+c4+c5;
+		comb5=perm4(word2);
+		for(int i=0;i<24;i++) {
+			list5.add(word1+comb5.get(i));
+		}
+		word1=c3;
+		word2=c1+c2+c4+c5;
+		comb5=perm4(word2);
+		for(int i=0;i<24;i++) {
+			list5.add(word1+comb5.get(i));
+		}
+		word1=c4;
+		word2=c1+c2+c3+c5;
+		comb5=perm4(word2);
+		for(int i=0;i<24;i++) {
+			list5.add(word1+comb5.get(i));
+		}
+		word1=c5;
+		word2=c1+c2+c3+c4;
+		comb5=perm4(word2);
+		for(int i=0;i<24;i++) {
+			list5.add(word1+comb5.get(i));
+		}
+		return list5;
+		
+	}
 	public static void main(String[] args) {
-		String s4="FAST";
-		perm4(s4);	
+		String s5="SMILE";
+		System.out.println(perm5("HELLO"));
+		
+		
 	}
 }
